54e067145a0e84bc607d1a7455a4273d5404ba21
MuhamadRohmatullah <m.rohmatullah10@gmail.com>
2024-05-15 09:44:58 +0700

    Menambahkan route mypost, myprofile, user-management, admin dan watch management

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,27 @@ Route::get('/', function () {
 Route::get('/register', function(){
     return view('user.register');
 });
+
+Route::get('/home', function(){
+    return view('user.home');
+});
+
+Route::get('/mypost', function(){
+    return view('user.posts');
+});
+
+Route::get('/myprofile', function(){
+    return view('user.myprofile');
+});
+
+Route::get('/user-management', function(){
+    return view('admin.user_management');
+});
+
+Route::get('/admin', function(){
+    return view('admin.dasboard');
+});
+
+Route::get('/watch-management', function(){
+    return view('admin.watch_management');
+});
