6750d94f07abca135188196d8ab6dcf1a1d36987
MuhamadRohmatullah <m.rohmatullah10@gmail.com>
2024-05-15 09:46:39 +0700

    menambahkan logo dan icon login

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-primary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">IBRA</a>
+    <img src="{{ asset('assets/images/logob.png') }}" alt="" style="width : 100px">
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -8,7 +8,7 @@
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
       </ul>
       <span class="navbar-text">
-      <button type="button" class="btn btn-outline-light" data-bs-toggle="modal" data-bs-target="#exampleModal">Login</button>
+      <button type="button" class="btn btn-outline-light" data-bs-toggle="modal" data-bs-target="#exampleModal"><img src="{{ asset('assets/images/login.png') }}" alt="" style="width : 20px">Login</button>
     </div>
   </div>
 </nav>
