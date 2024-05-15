ad6ac60cc19caa635f57448d39116136c6668445
MuhamadRohmatullah <m.rohmatullah10@gmail.com>
2024-05-15 09:51:06 +0700

    membuat modal untuk membuat postingan acara didalamnay ditambahkan form

---
+++ resources/views/components/post_form.blade.php
@@ -0,0 +1,68 @@
+<!-- Modal -->
+<div class="modal fade" id="exampleModal2" data-bs-backdrop="static" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Posting Acara Nobar</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Nama</label>
+                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Input Your name">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlInput2" class="form-label">Competition</label>
+                    <input type="email" class="form-control" id="exampleFormControlInput2" placeholder="name@example.com">
+                </div>
+                <div class="mb-3">
+                    <label for="inputDate" class="form-label">Date of Match</label>
+                    <input type="date" id="inputDate" class="form-control" aria-describedby="passwordHelpBlock">
+                </div>
+                <div class="mb-3">
+                    <div class="row">
+                        <div class="col">
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                                <label class="form-check-label" for="flexRadioDefault1">
+                                Free
+                                </label>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="flexRadioDefault2" id="flexRadioDefault2" checked>
+                                <label class="form-check-label" for="flexRadioDefault2">
+                                    Pay
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlInput3" class="form-label">Home team</label>
+                    <input type="email" class="form-control" id="exampleFormControlInput3" placeholder="+62">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlInput3" class="form-label">Away team</label>
+                    <input type="email" class="form-control" id="exampleFormControlInput3" placeholder="+62">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlTextarea1" class="form-label">Location</label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlInput3" class="form-label">Location in maps</label>
+                    <input type="email" class="form-control" id="exampleFormControlInput3" placeholder="+62">
+                </div>
+            </div>
+            <div class="modal-footer">
+                <div class="text-center mb-2">
+                    <button type="button" class="btn btn-primary" style="width : 100%;">Post</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+   
