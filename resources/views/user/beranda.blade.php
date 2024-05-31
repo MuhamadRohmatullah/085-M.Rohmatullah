fffeab36e507d60b758b7c9207381d3e8b822ef7
MuhamadRohmatullah <m.rohmatullah10@gmail.com>
2024-05-31 14:52:12 +0700

    (+) menambah halaman untuk postingan semua user

---
+++ resources/views/user/beranda.blade.php
@@ -0,0 +1,21 @@
+@extends("partial.layouts.app")
+
+@section("title", "beranda")
+
+@section("content")
+
+<div class="d-flex" id="wrapper">
+
+@include('partial.page.user.sidebar')
+    
+    <div id="page-content-wrapper" class="bg-light">
+       
+        @include('partial.page.user.navbar')
+
+        @include('partial.page.user.main')
+       
+
+    </div>
+</div>
+
+@endsection
